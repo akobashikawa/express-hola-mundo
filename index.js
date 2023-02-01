d55cd88e47f8c6af6138f1ac073eb1f48ef0d5a7
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-01 17:33:16 -0500

    Usando views

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const express = require('express');
 // Crear instancia de la aplicación Express
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 // Definir ruta para mostrar el mensaje
 app.get('/hola-mundo', (req, res) => {
-    res.send("Hola Mundo");
+    // res.send("Hola Mundo");
+    res.render('hola-mundo');
 });
 
+ // Escuchar peticiones en el puerto especificado
 app.listen(3000, () => {   console.log('Servidor iniciado en puerto 3000'); });
